e6f7ddcf63511031c0f6995c10bae50c53738aae
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2011-01-18 12:02:05 +0100

    make the upper rows negative, simplify elementAt

--- src/main/java/com/github/abalone/elements/Board.java
+++ src/main/java/com/github/abalone/elements/Board.java
@@ -19,41 +19,9 @@ public class Board implements Serializable {
 
    public Color elementAt(Coords coords) {
       Integer col = coords.getCol();
-      if (col < 1) {
+      Integer row = Math.abs(coords.getRow());
+      if (col < 1 || row > 4 || row + col > 9) {
          return Color.INVALID;
-      }
-      switch (coords.getRow()) {
-         case 1:
-         case 9:
-            if (col > 5) {
-               return Color.INVALID;
-            }
-            break;
-         case 2:
-         case 8:
-            if (col > 6) {
-               return Color.INVALID;
-            }
-            break;
-         case 3:
-         case 7:
-            if (col > 7) {
-               return Color.INVALID;
-            }
-            break;
-         case 4:
-         case 6:
-            if (col > 8) {
-               return Color.INVALID;
-            }
-            break;
-         case 5:
-            if (col > 9) {
-               return Color.INVALID;
-            }
-            break;
-         default:
-            return Color.INVALID;
       }
       Ball ball = new Ball(Color.WHITE, coords);
       if (this.balls.contains(ball)) {
@@ -68,16 +36,16 @@ public class Board implements Serializable {
 
    private Board() {
       for (int i = 1 ; i <= 5 ; ++i) {
-         this.addBall(new Ball(Color.WHITE, 1, i));
-         this.addBall(new Ball(Color.BLACK, 9, i));
+         this.addBall(new Ball(Color.WHITE, -4, i));
+         this.addBall(new Ball(Color.BLACK, 4, i));
       }
       for (int i = 1 ; i <= 6 ; ++i) {
-         this.addBall(new Ball(Color.WHITE, 2, i));
-         this.addBall(new Ball(Color.BLACK, 8, i));
+         this.addBall(new Ball(Color.WHITE, -3, i));
+         this.addBall(new Ball(Color.BLACK, 3, i));
       }
       for (int i = 3 ; i <= 5 ; ++i) {
-         this.addBall(new Ball(Color.WHITE, 3, i));
-         this.addBall(new Ball(Color.BLACK, 7, i));
+         this.addBall(new Ball(Color.WHITE, -2, i));
+         this.addBall(new Ball(Color.BLACK, 2, i));
       }
    }
 
