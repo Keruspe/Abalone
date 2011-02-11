44202d8a112877054f136c2700c7be13072d68a8
Sardem FF7 <sardemff7.pub@gmail.com>
2011-02-11 20:30:31 +0100

    Reindent

--- src/main/java/com/github/abalone/util/Coords.java
+++ src/main/java/com/github/abalone/util/Coords.java
@@ -7,99 +7,84 @@ import java.io.Serializable;
  * @author keruspe
  */
 public class Coords implements Serializable {
-   private Integer row;
-   private Integer col;
+    private Integer row;
+    private Integer col;
 
-   public Coords(Integer row, Integer col) {
-      this.row = row;
-      this.col = col;
-   }
+    public Coords(Integer row, Integer col) {
+        this.row = row;
+        this.col = col;
+    }
 
-   public Integer getRow() {
-      return this.row;
-   }
+    private Coords(Coords other) {
+        this.row = other.row;
+        this.col = other.col;
+    }
 
-   public Integer getCol() {
-      return this.col;
-   }
+    public Integer getRow() {
+        return this.row;
+    }
 
-   @Override
-   public boolean equals(Object obj) {
-      if (obj == null) {
-         return false;
-      }
-      if (getClass() != obj.getClass()) {
-         return false;
-      }
-      final Coords other = (Coords) obj;
-      if (this.row != other.row && (this.row == null || !this.row.equals(other.row))) {
-         return false;
-      }
-      if (this.col != other.col && (this.col == null || !this.col.equals(other.col))) {
-         return false;
-      }
-      return true;
-   }
+    public Integer getCol() {
+        return this.col;
+    }
 
-   public Coords moveTo( Direction direction){
-       // construction de la coordonee d arrivee
-       Coords arrivee = new Coords(0, 0);
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Coords other = (Coords) obj;
+        if (this.row != other.row && (this.row == null || !this.row.equals(other.row))) {
+            return false;
+        }
+        if (this.col != other.col && (this.col == null || !this.col.equals(other.col))) {
+            return false;
+        }
+        return true;
+    }
 
-       if(direction == Direction.UPLEFT){
+    public Coords moveTo(Direction direction){
+        // construction de la coordonee d arrivee
+        Coords arrivee = new Coords(this);
 
-           if (this.getRow()>1){
-                arrivee.row =this.getRow()+1;
-                arrivee.col = this.getCol();
-            }
-            else{
-                arrivee.row = this.getRow()+1;
-                arrivee.col = this.getCol()-1;
-            }
-           }
-           if(direction == Direction.UPRIGHT){
-                if (this.getRow()>1){
-                    arrivee.row = this.getRow()+1;
-                    arrivee.col = this.getCol()+1;
-                }else{
-                    arrivee.row = this.getRow()+1;
-                    arrivee.col = this.getCol();
-                }
-            }
-            if(direction == Direction.LEFT){
-                arrivee.row = this.getRow();
-                arrivee.col = this.getCol()-1;
-            }
-            if(direction == Direction.RIGHT){
+        switch ( direction )
+        {
+            case UPLEFT:
+                ++arrivee.row;
+                if ( this.row > 1 )
+                    --arrivee.col;
+            break;
+            case UPRIGHT:
+                ++arrivee.row;
+                if ( this.row > 1 )
+                    ++arrivee.col;
+            break;
+            case LEFT:
+                 --arrivee.col;
+            break;
+            case RIGHT:
+                 ++arrivee.col;
+            break;
+            case DOWNLEFT:
+                 --arrivee.row;
+                 if ( this.row > 1 )
+                    --arrivee.col;
+            break;
+            case DOWNRIGHT:
+                 ++arrivee.row;
+                 if ( this.row > 1 )
+                    ++arrivee.col;
+            break;
+        }
+        return arrivee;
 
-                arrivee.row = this.getRow();
-                arrivee.col = this.getCol()+1;
-            }
-            if(direction == Direction.DOWNLEFT){
+    }
 
-           if (this.getRow()>1){
-                arrivee.row = this.getRow()-1;
-                arrivee.col = this.getCol()-1;
-            }
-            else{
-                arrivee.row = this.getRow()-1;
-                arrivee.col = this.getCol();
-            }
-           }
-           if(direction == Direction.DOWNRIGHT){
-                if (this.getRow()>1){
-                    arrivee.row = this.getRow()+1;
-                    arrivee.col = this.getCol();
-                }else{
-                   arrivee.row = this.getRow()+1;
-                   arrivee.col = this.getCol();
-                }
-     }
-     return arrivee;
-
-   }
-   
-   @Override
-   public int hashCode() {
-      return this.row * 10 + this.col;
-   }
+    @Override
+    public int hashCode() {
+        return this.row * 10 + this.col;
+    }
 }
