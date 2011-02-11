0dbd1450ae5001276fa4f31adb72ae06b7184656
Sardem FF7 <sardemff7.pub@gmail.com>
2011-02-11 20:31:19 +0100

    Add selection support

--- src/main/java/com/github/abalone/view/Board.java
+++ src/main/java/com/github/abalone/view/Board.java
@@ -2,31 +2,40 @@ package com.github.abalone.view;
 
 import com.github.abalone.config.Config;
 import com.github.abalone.elements.Ball;
+import com.github.abalone.util.Color;
 import com.github.abalone.util.Coords;
 import com.kitfox.svg.app.beans.SVGIcon;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.URISyntaxException;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.HashSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.GroupLayout;
 import javax.swing.JPanel;
 
 /**
  *
  * @author sardemff7
  */
-class Board extends JPanel {
+class Board extends JPanel implements MouseListener
+{
     private final SVGIcon board;
     private Double boardScale = -1.0;
     private final SVGIcon whiteBall;
     private final SVGIcon blackBall;
     private Integer origX = 0;
     private Integer origY = 0;
+    private HashSet<Coords> selectedBalls;
+    private final SVGIcon selection;
 
     public Board()
     {
+        this.selectedBalls = new HashSet();
+        
         this.board = new SVGIcon();
         this.board.setScaleToFit(true);
         this.board.setAntiAlias(true);
@@ -36,6 +45,9 @@ class Board extends JPanel {
         this.blackBall = new SVGIcon();
         this.blackBall.setScaleToFit(true);
         this.blackBall.setAntiAlias(true);
+        this.selection = new SVGIcon();
+        this.selection.setScaleToFit(true);
+        this.selection.setAntiAlias(true);
         try
         {
             String theme = (String) Config.get("theme");
@@ -44,11 +56,14 @@ class Board extends JPanel {
             this.board.setSvgURI(getClass().getResource("game/" + theme + "/board.svg").toURI());
             this.whiteBall.setSvgURI(getClass().getResource("game/" + theme + "/white-ball.svg").toURI());
             this.blackBall.setSvgURI(getClass().getResource("game/" + theme + "/black-ball.svg").toURI());
+            this.selection.setSvgURI(getClass().getResource("game/" + theme + "/selection.svg").toURI());
         }
         catch (URISyntaxException ex)
         {
             Logger.getLogger(Board.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        this.addMouseListener(this);
     }
     
     void computeBoardScale(Boolean force)
@@ -82,6 +97,7 @@ class Board extends JPanel {
         Dimension ballSize = new Dimension((int)(100.0 * s), (int)(100.0 * s));
         this.whiteBall.setPreferredSize(ballSize);
         this.blackBall.setPreferredSize(ballSize);
+        this.selection.setPreferredSize(ballSize);
     }
     
     @Override
@@ -97,22 +113,80 @@ class Board extends JPanel {
         for (Ball b: com.github.abalone.elements.Board.getInstance().getBalls() )
         {
             Coords coords = b.getCoords();
+            Point point = this.getPoint(coords);
+            switch ( b.getColor() )
+            {
+                case WHITE:
+                    this.whiteBall.paintIcon(this, g, point.x, point.y);
+                break;
+                case BLACK:
+                    this.blackBall.paintIcon(this, g, point.x, point.y);
+                break;
+
+            }
+            if ( this.selectedBalls.contains(coords) )
+            {
+                this.selection.paintIcon(this, g, point.x, point.y);
+            }
+        }
+    }
+
+    private Point getPoint(Coords coords)
+    {
             Integer r = coords.getRow();
             Integer c = coords.getCol();
             Double bX = 180.0 + Math.abs(r) * 65.0 + c * 130.0;
             Double bY = (700.0 + 110.0 * r);
             Integer x = this.origX + (int)(bX * this.boardScale);
             Integer y = this.origY + (int)(bY * this.boardScale);
-            switch ( b.getColor() )
-            {
-                case WHITE:
-                    this.whiteBall.paintIcon(this, g, x, y);
-                break;
-                case BLACK:
-                    this.blackBall.paintIcon(this, g, x, y);
-                break;
 
-            }
-        }
+            return new Point(x, y);
+    }
+
+    private Coords getCoords(Point point)
+    {
+        Double bX = (double)point.x / this.boardScale - (double)this.origX;
+        Double bY = (double)point.y / this.boardScale - (double)this.origY;
+
+        Integer r = (int)(( bY - 750.0 ) / 100.0);
+        Integer c = (int)(( bX - 250.0 - Math.abs(r) * 65.0 ) / 130.0) - 2;
+
+        return new Coords(r, c);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me)
+    {
+        Coords coords = this.getCoords(me.getPoint());
+        Color color = com.github.abalone.elements.Board.getInstance().elementAt(coords);
+        if ( ( color != Color.WHITE ) && ( color != Color.BLACK ) )
+            return;
+        else if(this.selectedBalls.contains(coords))
+            this.selectedBalls.remove(coords);
+        else if ( this.selectedBalls.size() < 3 )
+            this.selectedBalls.add(coords);
+        else
+            return;
+        this.repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me)
+    {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me)
+    {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me)
+    {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me)
+    {
     }
 }
