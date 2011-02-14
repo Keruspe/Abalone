e086c5e9ed036ae8d8740d190a9f810bd9eb3a33
Sardem FF7 <sardemff7.pub@gmail.com>
2011-02-14 13:16:53 +0100

    Display valid moves buttons only

--- src/main/java/com/github/abalone/view/DirectionSelector.java
+++ src/main/java/com/github/abalone/view/DirectionSelector.java
@@ -3,6 +3,7 @@ package com.github.abalone.view;
 import com.github.abalone.util.Direction;
 import java.awt.GridLayout;
 import java.util.EnumMap;
+import java.util.Set;
 import javax.swing.JPanel;
 
 /**
@@ -13,7 +14,7 @@ public class DirectionSelector extends JPanel
 {
     private EnumMap<Direction, DirectionButton> buttons;
 
-    public DirectionSelector()
+    DirectionSelector(Board board)
     {
         this.setLayout(new GridLayout(3, 2));
 
@@ -21,9 +22,18 @@ public class DirectionSelector extends JPanel
         
         for ( Direction d: Direction.values() )
         {
-            DirectionButton b = new DirectionButton(d);
+            DirectionButton b = new DirectionButton(d, board);
+            b.setVisible(false);
             this.buttons.put(d, b);
             this.add(b);
         }
+    }
+
+    void updateButtons(Set<Direction> valids)
+    {
+        for ( DirectionButton b: this.buttons.values() )
+            b.setVisible(false);
+        for ( Direction d: valids )
+            this.buttons.get(d).setVisible(true);
     }
 }
