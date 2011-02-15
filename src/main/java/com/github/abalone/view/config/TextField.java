2a1025732ca6020d66f6e31d523deb4f99b5d91c
Sardem FF7 <sardemff7.pub@gmail.com>
2011-02-15 18:09:06 +0100

    Set value when TextField content change

--- src/main/java/com/github/abalone/view/config/TextField.java
+++ src/main/java/com/github/abalone/view/config/TextField.java
@@ -1,16 +1,27 @@
 package com.github.abalone.view.config;
 
 import com.github.abalone.config.Value;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JTextField;
 
 /**
  *
  * @author sardemff7
  */
-class TextField extends JTextField
+class TextField extends JTextField implements ActionListener
 {
+    private final Value value;
     public TextField(Value value)
     {
             super((String)value.get());
+            this.value = value;
+            this.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        this.value.set(this.getText());
     }
 }
