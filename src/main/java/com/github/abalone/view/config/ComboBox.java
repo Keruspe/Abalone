6b6a222827de34dfad6f55d9482adbe3f71b8b7d
Sardem FF7 <sardemff7.pub@gmail.com>
2011-02-15 18:17:35 +0100

    Set ComboBox values

--- src/main/java/com/github/abalone/view/config/ComboBox.java
+++ src/main/java/com/github/abalone/view/config/ComboBox.java
@@ -2,19 +2,31 @@ package com.github.abalone.view.config;
 
 import com.github.abalone.config.ConstraintValue;
 import com.github.abalone.config.Value;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JComboBox;
 
 /**
  *
  * @author sardemff7
  */
-class ComboBox extends JComboBox
+class ComboBox extends JComboBox implements ActionListener
 {
+    private final Value value;
+    
     ComboBox(Value value)
     {
+        this.value = value;
         for ( Object option: ((ConstraintValue)value).getList() )
         {
             this.addItem(option);
         }
+        this.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        this.value.set(this.getSelectedItem());
     }
 }
