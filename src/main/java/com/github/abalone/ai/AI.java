8b8641d69e35a46770c5a19af7eeced66ef6f19c
Sardem FF7 <sardemff7.pub@gmail.com>
2011-04-06 14:03:44 +0200

    NegaMax implementation

--- src/main/java/com/github/abalone/ai/AI.java
+++ src/main/java/com/github/abalone/ai/AI.java
@@ -1,21 +1,19 @@
 package com.github.abalone.ai;
 
-import com.github.abalone.controller.GameController;
-import com.github.abalone.elements.Ball;
-import com.github.abalone.elements.Game;
-import com.github.abalone.util.Color;
-import com.github.abalone.util.Coords;
-import com.github.abalone.util.Direction;
 import com.github.abalone.controller.Move;
 import com.github.abalone.elements.Board;
-import java.util.HashSet;
+import com.github.abalone.elements.Game;
+import com.github.abalone.util.Color;
 import java.util.Set;
 
 /**
  *
  * @author keruspe
+ * @author sardemff7
  */
 public class AI {
+
+   private static final Integer MAX_DEPTH = 3;
 
    private static AI instance;
    private Game game;
@@ -29,7 +27,6 @@ public class AI {
    public static void init(Game game, Color AIColor) {
       if (AI.instance == null)
          AI.instance = new AI(game, AIColor);
-      AI.instance.getBestMove(Color.WHITE);
    }
 
    public static AI getInstance() {
@@ -37,26 +34,43 @@ public class AI {
    }
 
    public Move getBestMove(Color current) {
+      Board board = new Board(this.game.getBoard());
       Move bestMove = null;
-      Board board = this.game.getBoard();
-      Set<Ball> balls = board.getBalls();
-      for (Ball b : balls) {
-         if (b.getColor() != current) {
-            continue;
+      Integer best = -100;
+      Set<Move> moves = board.getPossibleMoves(current);
+      for ( Move m : moves ) {
+         board.apply(m);
+         Integer score = negaMax(board, current.other(), MAX_DEPTH - 1);
+         if ( score > best ) {
+             best = score;
+             bestMove = m;
          }
-         Set<Coords> coords = new HashSet<Coords>();
-         coords.add(b.getCoords());
-         for ( Direction d : Direction.values() ) {
-            bestMove = null;
-            bestMove = new Move(board.getLineColorBallsAt(coords, current), d, current);
-            bestMove.compute(board);
-            if ( bestMove.isValid() )
-               break;
-         }
-         if ( bestMove != null )
-            break;
+         board.revert(m);
       }
       return bestMove;
+   }
+
+   private Integer negaMax(Board board, Color current, Integer depth) {
+      if ( depth > 0 ) {
+         Integer best = -100;
+         Set<Move> moves = board.getPossibleMoves(current);
+         for ( Move m : moves ) {
+            board.apply(m);
+            Integer score = negaMax(board, current.other(), depth - 1);
+            if ( score > best )
+                best = score;
+            board.revert(m);
+         }
+         return best;
+      } else {
+          return ( ( current == selfColor ) ? 1 : -1 ) * this.evaluateBoard(board, current);
+      }
+   }
+
+   private Integer evaluateBoard(Board board, Color player) {
+      Integer good = board.ballsCount(player);
+      Integer bad = board.ballsCount(player.other());
+      return good - bad;
    }
 
    public Color getColor() {
